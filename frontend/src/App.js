2a2817e390c1d61c0c1ec3cb901fe26381fca17a
sroy@corticacare.com <sroy@corticacare.com>
2023-11-04 01:00:46 -0700

    adding first API request from custom backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 //import logo from "./logo.svg";
 import "./App.css";
+import axios from "axios";
 
 // Dummy function for the visitor counter - in a real app this would be dynamic
 const useVisitorCounter = () => {
@@ -10,11 +11,25 @@ const useVisitorCounter = () => {
 function App() {
   const visitorCount = useVisitorCounter();
 
+  const [message, setMessage] = useState(""); // Correct usage of useState
+
+  useEffect(() => {
+    axios
+      .get("https://7zwpke1hy4.execute-api.us-west-1.amazonaws.com/Prod/hello")
+      .then((response) => {
+        setMessage(response.data.message); // Assuming your API response has a 'message' field
+      })
+      .catch((error) => {
+        console.error("There was an error fetching the data:", error);
+      });
+  }, []); // Empty dependency array means this effect runs once on component mount
+
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>Visitors: {visitorCount}</p>
+        <p>API Response: {message || "Loading..."}</p>
         <h1>Sam Roy</h1>
         <h2>Head of Technology</h2>
         <section className="Resume-section">
@@ -30,8 +45,7 @@ function App() {
           <h3>Skills</h3>
           <p>JavaScript, React, Node.js, etc.</p>
         </section>
-        <footer className="App-footer">
-        </footer>
+        <footer className="App-footer"></footer>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
