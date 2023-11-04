1540f94e76270937582ee62a252b4ddc47e7d1da
sroy@corticacare.com <sroy@corticacare.com>
2023-11-04 01:21:40 -0700

    adding cors to lambda func itself, im not happy about this

--- backend/src/hello_world/app.py
+++ backend/src/hello_world/app.py
@@ -4,5 +4,9 @@ def lambda_handler(event, context):
     # Return a simple "Hello, World!" response
     return {
         'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': '*',  # Required for CORS support to work
+            'Access-Control-Allow-Credentials': True  # Required for cookies, authorization headers with HTTPS 
+        },
         'body': json.dumps('Hello, World!')
     }
