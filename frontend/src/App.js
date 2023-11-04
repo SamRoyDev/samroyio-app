7df9c7b7d5e9763932c6a1b5d11df44c5f3aeab1
sroy@corticacare.com <sroy@corticacare.com>
2023-11-04 01:28:26 -0700

    componentize resume section and hide it for now

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 //import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
+import ResumeSection from './ResumeSection'; 
+
 
 // Dummy function for the visitor counter - in a real app this would be dynamic
 const useVisitorCounter = () => {
@@ -30,21 +32,7 @@ function App() {
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>Visitors: {visitorCount}</p>
         <p>API Response: {message || "Loading..."}</p>
-        <h1>Sam Roy</h1>
-        <h2>Head of Technology</h2>
-        <section className="Resume-section">
-          <h3>Experience</h3>
-          <p>Position at Company</p>
-          <p>More details about experience, projects, etc.</p>
-        </section>
-        <section className="Resume-section">
-          <h3>Education</h3>
-          <p>BS in Computer Science from State University</p>
-        </section>
-        <section className="Resume-section">
-          <h3>Skills</h3>
-          <p>JavaScript, React, Node.js, etc.</p>
-        </section>
+        {/* <ResumeSection /> */}
         <footer className="App-footer"></footer>
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ frontend/src/ResumeSection.js
@@ -0,0 +1,24 @@
+// ResumeSection.js
+function ResumeSection() {
+  return (
+    <>
+      <h1>Sam Roy</h1>
+      <h2>Head of Technology</h2>
+      <section className="Resume-section">
+        <h3>Experience</h3>
+        <p>Position at Company</p>
+        <p>More details about experience, projects, etc.</p>
+      </section>
+      <section className="Resume-section">
+        <h3>Education</h3>
+        <p>BS in Computer Science from State University</p>
+      </section>
+      <section className="Resume-section">
+        <h3>Skills</h3>
+        <p>JavaScript, React, Node.js, etc.</p>
+      </section>
+    </>
+  );
+}
+
+export default ResumeSection;
