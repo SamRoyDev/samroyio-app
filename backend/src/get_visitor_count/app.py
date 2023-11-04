dc17085fcabc40b8aa508f8ee9cb661323ca85f2
sroy@corticacare.com <sroy@corticacare.com>
2023-11-04 09:51:08 -0700

    adding get visitor count func

---
+++ backend/src/get_visitor_count/app.py
@@ -0,0 +1,34 @@
+# src/store_visitor/app.py
+import json
+import boto3
+from datetime import datetime
+
+# Initialize DynamoDB client
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('VisitorCount')
+
+def lambda_handler(event, context):
+    # Store the visit in DynamoDB
+    try:
+        response = table.scan(
+            Select='COUNT'
+        )
+        count = response['Count']
+
+        while 'LastEvaluatedKey' in response:
+            response = table.scan(
+                Select='COUNT',
+                ExclusiveStartKey=response['LastEvaluatedKey']
+            )
+            count += response['Count']
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'count': count})
+        }
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Unable to retrieve count of visitors.')
+        }
