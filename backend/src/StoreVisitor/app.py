a5512d8b581b70f2e95552cd074586aa0b479587
sroy@corticacare.com <sroy@corticacare.com>
2023-11-04 09:26:32 -0700

    adding new store visitor func

---
+++ backend/src/StoreVisitor/app.py
@@ -0,0 +1,34 @@
+# src/store_visitor/app.py
+import json
+import boto3
+import uuid
+from datetime import datetime
+
+# Initialize DynamoDB client
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('VisitorCount')
+
+def lambda_handler(event, context):
+    # Generate a unique visitor ID
+    visitor_id = str(uuid.uuid4())
+    # Get the current timestamp
+    timestamp = str(datetime.utcnow().isoformat())
+
+    # Store the visit in DynamoDB
+    try:
+        table.put_item(
+            Item={
+                'VisitorId': visitor_id,
+                'Timestamp': timestamp
+            }
+        )
+        return {
+            'statusCode': 200,
+            'body': json.dumps('Visitor recorded successfully.')
+        }
+    except Exception as e:
+        print(e)
+        return {
+            'statusCode': 500,
+            'body': json.dumps('Error saving the visitor.')
+        }
