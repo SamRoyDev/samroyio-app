25341269d259a9548d572a0dce165d5e50008096
sroy@corticacare.com <sroy@corticacare.com>
2023-11-04 21:14:42 -0700

    componentizing navigation

---
+++ frontend/src/Navigation.js
@@ -0,0 +1,18 @@
+function Navigation() {
+  return (
+    <>
+      <nav className="App-nav">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Resume</li>
+          <li>Services</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navigation;
