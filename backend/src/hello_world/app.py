53f51ac2893d4564e70944524c159b460e725f6d
sroy@corticacare.com <sroy@corticacare.com>
2024-02-19 21:45:56 -0800

    changed hello world to True

--- backend/src/hello_world/app.py
+++ backend/src/hello_world/app.py
@@ -1,12 +1,12 @@
 import json
 
 def lambda_handler(event, context):
-    # Return a simple "Hello, World!" response
+    # Return a simple `True` response if API is online
     return {
         'statusCode': 200,
         'headers': {
             'Access-Control-Allow-Origin': '*',  # Required for CORS support to work
             'Access-Control-Allow-Credentials': True  # Required for cookies, authorization headers with HTTPS 
         },
-        'body': json.dumps('Hello, World!')
+        'body': json.dumps(True)
     }
