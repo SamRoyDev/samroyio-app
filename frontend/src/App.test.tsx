c44548eeebe2f481c91803d78b57eaf83ba0758e
sroy@corticacare.com <sroy@corticacare.com>
2024-02-20 09:50:56 -0800

    fixed tests

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("make sure that samroy.io is rending", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const textElement = screen.getByText(/samroy.io/i);
+  expect(textElement).toBeInTheDocument();
 });
