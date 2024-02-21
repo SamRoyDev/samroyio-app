adbcdb09da224450dc55ca82f1970bb1007e85f1
sroy@corticacare.com <sroy@corticacare.com>
2024-02-21 10:50:22 -0800

    updating nav

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -3,13 +3,21 @@ import React from "react";
 const Navigation: React.FC = () => {
   return (
     <>
-      <nav className="App-nav">
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#api">API</a></li>
-        </ul>
-      </nav>
+      <div className="App-nav">
+        <nav className="App-nav">
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#projects">Projects</a>
+            </li>
+            <li>
+              <a href="#api">API</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </>
   );
 };
