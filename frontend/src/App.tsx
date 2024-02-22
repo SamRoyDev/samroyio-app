4db56cb581f5d76e0ff2901e7c82d1804009d022
sroy@corticacare.com <sroy@corticacare.com>
2024-02-21 22:13:15 -0800

    main app add docs section and supporting markdown packages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import "./css/App.css";
 import "./css/Navigation.css";
 import "./css/GithubRepositories.css";
+import "./css/ApiSection.css";
+import "./css/Markdown.css";
 //import ResumeSection from "./components/ResumeSection";
 import Navigation from "./components/Navigation";
 import Api from "./components/Api";
 import GithubRepositories from "./components/GithubRepositories";
 import TopButton from "./components/TopButton";
+import MarkdownRenderer from "./components/MarkdownRenderer";
 
 const App: React.FC = () => {
   return (
@@ -16,16 +19,26 @@ const App: React.FC = () => {
         <div className="brand">
           <p>sam roy</p>
         </div>
-        <div className="api">{/*<Api />*/}</div>
+        <Api />
       </header>
       <Navigation />
       <main className="App-main">
         <br></br>
+        <section id="documentation">
+          <h1>Documentation</h1>
+          <div className="markdown-body">
+            <MarkdownRenderer filePath="./markdown/app-docs.md" />
+          </div>
+        </section>
         <section id="projects">
+        <hr />
+        <h1>Projects</h1>
           <GithubRepositories />
         </section>
         {/* <ResumeSection /> */}
         <section id="api">
+        <hr />
+        <h1>API</h1>
           <Api />
         </section>
       </main>
